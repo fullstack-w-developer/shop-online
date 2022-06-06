e63673461a08c24180553fc4a537377d3557568c
mahdi-shariflo <mahdilove279@gmail.com>
2022-06-06 08:33:36 +0430

    fixed url

--- next.config.js
+++ next.config.js
@@ -4,13 +4,13 @@ module.exports = {
   env: {
     MONGO_URi:
       "mongodb+srv://admin:m1a2h3d4i5@cluster0.gy9ht.mongodb.net/shops?retryWrites=true&w=majority",
-    BASE_Url: "https://git.heroku.com/shopsonline.git",
-    ACCESS_TOKEN_SECRET:
-      "uhcuyfgwiydff4873iykr87fhuiaalh849ur984yrfhuyg3",
-    REFRESH_TOKEN_SECRET:
-      "jkfe3r7fyhcbuyjsgx87wuf3qaw0x8fhx7rcfgf6c7fgcbcg8545bf7",
+    BASE_Url: "http://localhost:3000",
+    ACCESS_TOKEN_SECRET:"uhcuyfgwiydff4873iykr87fhuiaalh849ur984yrfhuyg3",
+    REFRESH_TOKEN_SECRET:"jkfe3r7fyhcbuyjsgx87wuf3qaw0x8fhx7rcfgf6c7fgcbcg8545bf7"
   },
   images: {
-    domains: ["dkstatics-public.digikala.com"],
+    domains: [
+      "dkstatics-public.digikala.com",
+    ],
   },
 };
